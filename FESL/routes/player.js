b6d165ff3de4783d106d10ba1efab3616e9382b9
Justin Long <the_queso_loco@hotmail.com>
2014-04-21 13:30:25 -0600

    update to move to BREAD structure

--- FESL/routes/player.js
+++ FESL/routes/player.js
@@ -1,10 +1,10 @@
 ﻿var db = require('../db/db')('player');
 
 exports.browse = function (req, res) {
-	res.render('player', db.read('Faker'));
+	res.render('player/read', db.read('Faker'));
 }
 
 exports.read = function (req, res) {
 	var playerId = req.params.id;
-	res.render('player', db.read(playerId));
+	res.render('player/read', db.read(playerId));
 }
